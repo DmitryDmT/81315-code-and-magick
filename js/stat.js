983c68f0da4d0c7c2fa3c59aae6233bc55a520a8
DmitryDmD <deloakk.mr@mail.ru>
2017-11-21 19:21:44 +0300

    Добавление гистограммы

--- js/stat.js
+++ js/stat.js
@@ -1,69 +1,78 @@
 window.renderStatistics = function(ctx, names, times) {
+  var getRandomColor = function() {
+    return Math.random();
+  };
+
+  var barWidth = 40;
+  var initialBarX = 130;
+  var initialBarY = 250;
+  var initialTextX = 130;
+  var initialTextY = 260;
+  var marginBar = 100;
+  var marginText = 100;
+
+  var getMainPersonBar = function() {
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    ctx.fillRect(initialBarX + marginBar * i, initialBarY, barWidth, times[i] * step);
+    ctx.fill();
+    ctx.fillStyle = '#000000';
+    ctx.fillText(names[i], initialTextX + marginText * i, initialTextY);
+    ctx.closePath();
+  };
+
+  var getOtherPersonsBar = function() {
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(16, 7, 133, ' + getRandomColor() + ')';
+    ctx.fillRect(initialBarX + marginBar * i, initialBarY, barWidth, times[i] * step);
+    ctx.fill();
+    ctx.fillStyle = '#000000';
+    ctx.fillText(names[i], initialTextX + marginText * i, initialTextY);
+    ctx.closePath();
+  };
+
+  var getTimesBar = function(times) {
+    for(var i = 0; i < times.length; i++) {
+      var time = times[i];
+
+      if (time > maxTime) {
+        maxTime = time;
+      }
+    }
+  };
+  
   ctx.beginPath();
-  ctx.rect(110, 20, 420, 270);
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.rect(110, 20, 420, 270);
   ctx.fill();
   ctx.closePath();
-  
+
   ctx.beginPath();
   ctx.rect(100, 10, 420, 270);
   ctx.fillStyle = '#ffffff';
   ctx.fill();
   ctx.closePath();
-  
+
+  ctx.beginPath();
+  ctx.fillStyle = '#000000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
-  ctx.fillStyle = '#000000';
   ctx.fillText('Ура вы победили!', 115, 30);
   ctx.fillText('Список результатов:', 115, 50);
-  
+  ctx.closePath();
+
   var maxTime = -1;
-  var indexName = -1;
-  
+  var statsHeight = -150;
+
+  getTimesBar(times);
+
+  var step = statsHeight / (maxTime - 0);
+
   for(var i = 0; i < times.length; i++) {
-    var time = times[i];
-    
-    if (time > maxTime) {
-      maxTime = time;
-      indexName = i;
+    if (names[i] === 'Вы') {
+      getMainPersonBar();
+    } else {
+      getOtherPersonsBar();
     }
   }
-  
-//  ctx.beginPath();
-//  ctx.rect(130, 100, 40, 150);
-//  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-//  ctx.fill();
-//  ctx.closePath();
-//  
-//  ctx.beginPath();
-//  ctx.rect(220, 100, 40, 150);
-//  ctx.fillStyle = '#100785';
-//  ctx.fill();
-//  ctx.closePath();
-//  
-//  ctx.beginPath();
-//  ctx.rect(310, 100, 40, 150);
-//  ctx.fillStyle = 'rgba(16, 7, 133, 1)';
-//  ctx.fill();
-//  ctx.closePath();
-//  
-//  ctx.beginPath();
-//  ctx.rect(400, 100, 40, 150);
-//  ctx.fillStyle = 'rgba(16, 7, 133, 1)';
-//  ctx.fill();
-//  ctx.closePath();
-  
-  var statsHeight = -150;
-  var step = statsHeight / (maxTime - 0);
-  
-  for(var i = 0; i < times.length; i++) {
-    ctx.beginPath();
-    ctx.fillRect(130 + 100 * i, 250, 40, times[i] * step);
-    ctx.fillStyle = 'yellow';
-    ctx.fill();
-    ctx.fillStyle = 'red';
-    ctx.fillText(names[i], 130 + 100 * i, 260);
-    ctx.fill();
-    ctx.closePath();
-  }
-}
+};
