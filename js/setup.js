8cbc4ee4c5426d5e6eee66647d9cdbfa296559fa
DmitryDmD <deloakk.mr@mail.ru>
2017-11-30 19:30:10 +0300

    Добавление похожих персонажей

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,7 @@
+'use strict';
+
 function namesCompare () {
-  for (var i = 0; i < firstNamesArr.length; i++) {
+  for (var nameIndex = 0; nameIndex < firstNamesArr.length; nameIndex++) {
     var randomNumber = Math.floor(Math.random() * firstNamesArr.length);
     var heroNames = firstNamesArr[randomNumber] + ' ' + secondNamesArr[randomNumber];
     
@@ -8,7 +10,7 @@ function namesCompare () {
 }
 
 function colorCoatChoose () {
-  for (var i = 0; i < colorCoatsArr.length; i++) {
+  for (var colorCoatIndex = 0; colorCoatIndex < colorCoatsArr.length; colorCoatIndex++) {
     var randomColor = Math.floor(Math.random() * colorCoatsArr.length);
     var color = colorCoatsArr[randomColor];
     
@@ -17,7 +19,7 @@ function colorCoatChoose () {
 }
 
 function colorEyeChoose () {
-  for (var i = 0; i < colorEyesArr.length; i++) {
+  for (var colorEyeIndex = 0; colorEyeIndex < colorEyesArr.length; colorEyeIndex++) {
     var randomColor = Math.floor(Math.random() * colorEyesArr.length);
     var color = colorEyesArr[randomColor];
     
@@ -25,17 +27,11 @@ function colorEyeChoose () {
   }
 }
 
-
-var setupBlock = document.querySelector('.setup');
-
-
 var firstNamesArr = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var secondNamesArr = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 
 var colorCoatsArr = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-
 var colorEyesArr = ['black', 'red', 'blue', 'yellow', 'green'];
-
 
 var wizards = [
   {
@@ -60,6 +56,29 @@ var wizards = [
   }
 ];
 
-var template = document.querySelector('#similar-wizard-template').content;
+var setupBlock = document.querySelector('.setup');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var setupSimilarList = document.querySelector('.setup-similar-list');
+
+var fragment = document.createDocumentFragment();
+
+var renderWizard = function (wizard) {
+  var wizardPerson = similarWizardTemplate.cloneNode(true);
+
+  wizardPerson.querySelector('.setup-similar-label').textContent = wizard.names;
+  wizardPerson.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardPerson.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  
+  return wizardPerson;
+}
 
 setupBlock.classList.remove('hidden');
+
+for (var stepClone = 0; stepClone < wizards.length; stepClone++) {
+  fragment.appendChild(renderWizard(wizards[stepClone]));
+}
+
+setupSimilarList.appendChild(fragment);
+
+document.querySelector('.setup-similar').classList.remove('hidden');
